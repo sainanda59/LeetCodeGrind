531c2cffff217435c955b6e584f0a32b5a47e763
Sai Soumyak Nanda <74289464+sainanda59@users.noreply.github.com>
2022-11-29 23:42:41 +0530

    Time: 274 ms (29.79%), Space: 53.6 MB (15.45%) - LeetHub

--- 0888-fair-candy-swap/0888-fair-candy-swap.cpp
+++ 0888-fair-candy-swap/0888-fair-candy-swap.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes) {
-        unordered_map<int,int>ump1,ump2;
+        unordered_map<int,int>ump1;
         int sum1=0,sum2=0;
         for(auto it:aliceSizes){
             sum1+=it;
@@ -9,27 +9,11 @@ public:
         }
         for(auto it:bobSizes){
             sum2+=it;
-            ump2[it]++;
         }
-        int diff = abs(sum1-sum2);
-        int req = diff/2;
-        if(sum1==sum2){
-           for(auto it:ump1){
-               if(ump2[it.first]>0)
-                   return {it.first,it.first};
-           } 
-        }
-        else if(sum1>sum2){
-            for(auto it:ump1){
-                if(ump2[it.first-req]>0)
-                    return {it.first,it.first-req};
-            }
-        }
-        else{
-            for(auto it:ump1){
-                if(ump2[it.first+req]>0)
-                    return {it.first,it.first+req};
-            }
+        int diff = (sum1-sum2)/2;
+        for(auto it:bobSizes){
+           if(ump1[it+diff]>0)
+               return {it+diff,it};
         }
         return {-1,-1};
     }
