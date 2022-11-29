f7325d6f0e6b996e0c8d7d12589ce9a6aaca79dc
Sai Soumyak Nanda <74289464+sainanda59@users.noreply.github.com>
2022-11-29 23:53:43 +0530

    Added solution - LeetHub

---
+++ Find Index - GFG/find-index.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <iostream>
+#include <bits/stdc++.h> 
+#include <vector> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    vector<int> findIndex(int a[], int n, int key)
+    {
+        int left=-1;
+        int right=-1;
+        int chk=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==key){
+                chk=1;
+                if(left==-1 && right==-1){
+                    left=i;
+                    right=i;
+                }
+                else{
+                    right=i;
+                }
+            }
+        }
+        return {left,right};
+    }
+  
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        vector<int> res;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        int key;
+        cin>>key;
+        Solution ob;
+        res=ob.findIndex(arr, n, key);
+        for (int i = 0; i < res.size(); i++) 
+        cout << res[i] << " ";
+        cout << "\n";
+    }
+    
+    return 0;
+}
+
+// } Driver Code Ends
