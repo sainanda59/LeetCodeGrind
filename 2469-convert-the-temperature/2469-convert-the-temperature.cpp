5a50682295fca9c6eeca4e7d2b121be8e856e005
Sai Soumyak Nanda <74289464+sainanda59@users.noreply.github.com>
2022-12-02 00:09:49 +0530

    Time: 2 ms (44.45%), Space: 6 MB (60.84%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double a = celsius+273.15;
+        double b = celsius*(1.80)+32.00;
+        return {a,b};
+    }
+};
